d5244bce9bae0f567d893bbcb964f7b761490bc7
Timo Salmi <v-tisal@microsoft.com>
2016-07-25 15:51:52 +0300

    db

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `viski`.`evaluation` (
+  `product` INT NOT NULL,
+  `savuisuus` INT NULL,
+  `vaniljaisuus` INT NULL,
+  `kukkaisuus` INT NULL,
+  `mausteisuus` INT NULL,
+  `maltaisuus` INT NULL,
+  `makeus` INT NULL,
+  `miellyttavyys` INT NULL,
+  `login` VARCHAR(24) NULL,
+  PRIMARY KEY (`product`));
+
+ALTER TABLE `viski`.`evaluation` 
+CHANGE COLUMN `login` `login` VARCHAR(24) NOT NULL ,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`product`, `login`);
